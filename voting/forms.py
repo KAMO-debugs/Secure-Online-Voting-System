c387aea8241f80f65458e3924c993efd264c188d
Onwaba <onwabambanga@gmail.com>
2026-09-13 22:59:14 +0200

    Update registration and student forms

--- voting/forms.py
+++ voting/forms.py
@@ -55,3 +55,45 @@ class RegistrationForm(UserCreationForm):
             )
 
         return student_number
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError(
+                'An account with this email already exists.'
+            )
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        
+        if commit:
+            user.save()
+            
+            # Link student profile to user
+            student_number = self.cleaned_data['student_number']
+            student = StudentProfile.objects.get(student_number=student_number)
+            student.user = user
+            student.save()
+            
+        return user
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = StudentProfile
+        fields = ['student_number', 'full_name', 'campus', 'faculty']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Make student_number read-only
+        if self.instance.pk:
+            self.fields['student_number'].widget.attrs['readonly'] = True
+
+
+class StudentImportForm(forms.Form):
+    excel_file = forms.FileField(
+        label='Select Excel File',
+        help_text='Upload an .xlsx file with student details'
+    )
